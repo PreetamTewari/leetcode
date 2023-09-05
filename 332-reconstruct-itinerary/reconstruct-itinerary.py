eb5abc09ae7ba17182f9ade84bf029c2e386bd98
PreetamTewari <preetam.k.1158@gmail.com>
2023-09-05 23:49:08 +0530

    Time: 91 ms (58.86%) | Memory: 17.8 MB (8.02%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/reconstruct-itinerary.py
@@ -1,12 +1,12 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         tickets.sort()
-        adj = {src: [] for src, dst in tickets}
-        for src, dst in tickets:
-            adj[src].append(dst)
+        adj = {src: deque() for src, dst in tickets}
+        for s, d in tickets:
+            adj[s].append(d)
 
         res = ["JFK"]
-        
+
         def dfs(src):
             if len(res) == len(tickets) + 1:
                 return True
@@ -16,10 +16,10 @@ class Solution:
             temp = list(adj[src])
             for i, v in enumerate(temp):
                 res.append(v)
-                adj[src].pop(i)
-                if dfs(v): return True
+                adj[src].popleft()
+                if dfs(v) : return True
                 res.pop()
-                adj[src].insert(i, v)
+                adj[src].append(v)
             return False
         dfs("JFK")
         return res
