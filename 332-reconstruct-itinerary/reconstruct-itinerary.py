08930f71bdae37a6ec1600aadd9a6776d5c91d99
PreetamTewari <preetam.k.1158@gmail.com>
2023-09-05 23:45:01 +0530

    Time: 94 ms (48.45%) | Memory: 17.3 MB (34.88%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/reconstruct-itinerary.py
@@ -4,20 +4,22 @@ class Solution:
         adj = {src: [] for src, dst in tickets}
         for src, dst in tickets:
             adj[src].append(dst)
+
         res = ["JFK"]
+        
         def dfs(src):
             if len(res) == len(tickets) + 1:
                 return True
             if src not in adj:
                 return False
+
             temp = list(adj[src])
             for i, v in enumerate(temp):
-                adj[src].pop(i)
                 res.append(v)
-                if dfs(v):
-                    return True
-                adj[src].insert(i, v)
+                adj[src].pop(i)
+                if dfs(v): return True
                 res.pop()
+                adj[src].insert(i, v)
             return False
         dfs("JFK")
         return res
