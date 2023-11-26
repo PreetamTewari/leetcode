aa570432d12fd05cda284e1472e5c029c6b8dfb2
PreetamTewari <preetam.k.1158@gmail.com>
2023-11-26 22:51:31 +0530

    Time: 58 ms (90.91%) | Memory: 16.6 MB (7.76%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -7,28 +7,31 @@ class Solution:
         for i in range(len(s1)):
             c1[ord(s1[i]) - ord('a')] += 1
             c2[ord(s2[i]) - ord('a')] += 1
+        
+        l = 0
         matches = 0
         for i in range(26):
-            matches += 1 if c1[i] == c2[i] else 0
-
-        l = 0
-        for r in range(len(s1), len(s2)):
+            if c1[i] == c2[i]:
+                matches += 1
+        
+        for r in range(len(s1),len(s2)):
             if matches == 26:
                 return True
 
             index = ord(s2[r]) - ord('a')
             c2[index] += 1
 
-            if c1[index] == c2[index]:
+            if c2[index] == c1[index]:
                 matches += 1
             elif c1[index] + 1 == c2[index]:
                 matches -= 1
-
+            
             index = ord(s2[l]) - ord('a')
             c2[index] -= 1
+
             if c1[index] == c2[index]:
                 matches += 1
-            elif c1[index] - 1== c2[index]:
+            elif c1[index] - 1 == c2[index]:
                 matches -= 1
             l += 1
         return matches == 26
