a62bf660d59a7cb81f26e23c3f6e8e1706fa02d6
PreetamTewari <preetam.k.1158@gmail.com>
2023-11-26 22:57:21 +0530

    Time: 106 ms (72.69%) | Memory: 17.2 MB (9.78%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,28 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
-            return ""
+        if len(t) > len(s): return ""
+        cS, cT = {}, {}
 
-        cT, cS = {}, {}
         for c in t:
             cT[c] = 1 + cT.get(c, 0)
+        
         have, need = 0, len(cT)
-        res, resLen = [-1, -1], float('inf')
+        res, resLen = [-1, -1], float("inf")
         l = 0
         for r in range(len(s)):
-            c = s[r]
-            cS[c] = 1 + cS.get(c, 0)
+            cS[s[r]] = 1 + cS.get(s[r],0)
 
-            if c in cT and cT[c] == cS[c]:
+            if s[r] in cT and cS[s[r]] == cT[s[r]]:
                 have += 1
 
             while have == need:
                 if (r - l + 1) < resLen:
-                    res = [l, r]
                     resLen = r - l + 1
+                    res = [l, r]
+
                 cS[s[l]] -= 1
-                if s[l] in cT and cT[s[l]] > cS[s[l]]:
+                if s[l] in cT and cS[s[l]] < cT[s[l]]:
                     have -= 1
                 l += 1
         l, r = res
         return s[l:r+1]
+        
