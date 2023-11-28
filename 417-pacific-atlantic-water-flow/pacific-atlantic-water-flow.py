76627965c4ca335108f6d4fc71c1bbbb84b4cd76
PreetamTewari <preetam.k.1158@gmail.com>
2023-11-28 10:39:11 +0530

    Time: 255 ms (39.00%) | Memory: 17.9 MB (63.10%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,28 +1,31 @@
 class Solution:
     def pacificAtlantic(self, h: List[List[int]]) -> List[List[int]]:
-        pac, atl = set(), set()
         ROWS, COLS = len(h), len(h[0])
+        pac, atl = set(), set()
 
-        def dfs(r, c, visit,prevH):
-            if r not in range(ROWS) or c not in range(COLS) or (r, c) in visit or h[r][c] < prevH:
-                return
+        
+
+        def dfs(r, c, visit, prev):
+            if r < 0 or c < 0 or (r, c) in visit or r == ROWS or c == COLS or h[r][c] < prev:
+                return False
+
             visit.add((r, c))
             dfs(r+1, c, visit, h[r][c])
             dfs(r-1, c, visit, h[r][c])
             dfs(r, c+1, visit, h[r][c])
             dfs(r, c-1, visit, h[r][c])
 
-        for c in range(COLS):
-            dfs(0, c, pac, h[0][c])
-            dfs(ROWS-1, c, atl, h[ROWS-1][c])
-
         for r in range(ROWS):
             dfs(r, 0, pac, h[r][0])
             dfs(r, COLS - 1, atl, h[r][COLS - 1])
+        
+        for c in range(COLS):
+            dfs(0, c, pac, h[0][c])
+            dfs(ROWS - 1, c, atl, h[ROWS - 1][c])
 
-        out = []
+        res = []
         for r in range(ROWS):
             for c in range(COLS):
-                if (r, c) in pac and (r, c) in atl:
-                    out.append([r, c])
-        return out
+                if (r, c) in atl and (r, c) in pac:
+                    res.append([r, c])
+        return res
