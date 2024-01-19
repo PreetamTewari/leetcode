8f3ee8342ad13d89cc8bd5933f3369c579fa7049
PreetamTewari <preetam.k.1158@gmail.com>
2024-01-19 20:51:30 +0530

    Time: 35 ms (73.17%) | Memory: 16.6 MB (53.87%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -6,8 +6,8 @@ class Solution:
             if n == 0:
                 return 1
 
-            res = helper(x, n // 2)
-            res = res * res
+            res = helper(x * x, n // 2)
+            # res = res * res
             return x * res if n % 2 else res
 
 
