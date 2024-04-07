5ec7a854390d8eda8f2668ab2125d84948f61150
PreetamTewari <preetam.k.1158@gmail.com>
2024-04-07 19:17:46 +0530

    Time: 51 ms (49.38%) | Memory: 17 MB (34.22%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        if len(s) != len(t):
+            return False
+        cS, cT = {}, {}
+        for i in range(len(s)):
+            cS[s[i]] = 1 + cS.get(s[i], 0)
+            cT[t[i]] = 1 + cT.get(t[i], 0)
+        return cS == cT
